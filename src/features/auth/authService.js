997c26096db2a58dc4a5250f7bef642cc1542336
ashwanthdurairaj <ashwanthdurairaj2003@gmail.com>
2023-07-03 22:06:20 +0530

    update auth and goal services

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -4,7 +4,7 @@ const API_URL = 'https://supermarket-api-a8az.vercel.app/api/users/'
 
 // Register user
 const register = async (userData) => {
-  const response = await axios.post(API_URL, userData)
+  const response = await axios.post('https://supermarket-api-a8az.vercel.app/api/users', userData)
 
   if (response.data) {
     localStorage.setItem('user', JSON.stringify(response.data))
@@ -15,7 +15,7 @@ const register = async (userData) => {
 //lolol
 // Login user
 const login = async (userData) => {
-  const response = await axios.post(API_URL + 'login', userData)
+  const response = await axios.post('https://supermarket-api-a8az.vercel.app/api/users/login', userData)
 
   if (response.data) {
     localStorage.setItem('user', JSON.stringify(response.data))
